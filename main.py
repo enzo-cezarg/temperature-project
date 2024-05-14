88448bc141c797b9fec253a1136ec5be50690af1
Enzo <enzoocezargarcia@gmail.com>
2024-05-14 15:04:01 -0300

    first-phase-v0.1

---
+++ main.py
@@ -0,0 +1,34 @@
+# Inicialização das variáveis que armazenarão valores-chave
+contMeses = 0
+maiorTemp = 0
+menorTemp = 50
+mesMenorTemp = 0
+mesMaiorTemp = 0
+
+# Declaração de um array contendo os meses do ano
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+# Inicialização do laço de repetição que irá garantir que serão inseridos valores válidos
+while contMeses != 12:
+    # Output formatado adequadamente para o usuário
+    print('===='*20)
+    temperatura = float(input(f'Digite a temperatura para o mês de {meses[contMeses]} ({contMeses+1}) em graus Celsius: '))
+
+    # Verificação do intervalo da temperatura
+    if not(temperatura >= -60 and temperatura <= 50):
+        print('Inválido! Temperatura deve estar entre -60 °C e 50 °C')
+        continue
+    else:
+        print('Registrado!')
+        # Verificação da maior e da menor temperatura
+        if temperatura > maiorTemp:
+            maiorTemp = temperatura
+            mesMaiorTemp = contMeses
+        elif temperatura < menorTemp:
+            menorTemp = temperatura
+            mesMenorTemp = contMeses
+        contMeses += 1
+        
+# Output final do programa
+print(f'Maior temperatura lida: {maiorTemp:.1f} °C | Mês: {meses[mesMaiorTemp]}')
+print(f'Menor temperatura lida: {menorTemp:.1f} °C | Mês: {meses[mesMenorTemp]}')
