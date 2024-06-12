66ffa1a50e467fc5c02acf7eea3baf057fe4fcc3
Enzo <enzoocezargarcia@gmail.com>
2024-06-12 16:35:19 -0300

    second-phase-v2.0

--- second-phase/main.py
+++ second-phase/main.py
@@ -3,6 +3,8 @@
 # | Escrito por Enzo Cezar Garcia Rocha com início em 07/06/2024                           | #
 # # ====================================================================================== # #
 
+import matplotlib.pyplot as plt
+
 # O arquivo fornecido teve seu nome alterado para "dados.csv" apenas por 
 # motivos de praticidade, seu conteúdo não foi modificado
 def carregarDados(arq):
@@ -43,7 +45,9 @@ def validaMes(mes):
         return False
     
 def validaAno(ano, mes):
+    """Valida se o ano está no intervalo permitido"""
     if ano >= 1961 and ano <= 2016:
+        # Registros de 2016 vão somente até julho, por isso essa providência
         if ano == 2016 and mes > 7:
             print('Mês inexistente na base de dados! Tente novamente.\n')
             return False
@@ -54,6 +58,7 @@ def validaAno(ano, mes):
         return False
 
 def validaEntrada():
+    """Valida cada mês e ano digitados pelo usuário, permitindo a redigitação caso necessário"""
     validaMesI = False
     while validaMesI == False:
         mesI = int(input('Digite o mês inicial (1 a 12): '))
@@ -77,6 +82,7 @@ def validaEntrada():
                                     return [mesI, anoI, mesF, anoF]
 
 def ordenarAnos(mesI, anoI, mesF, anoF):
+    """Ordena de forma que os anos menores fiquem sempre como iniciais"""
     if anoI > anoF:
         aux = anoF
         anoF = anoI
@@ -86,6 +92,7 @@ def ordenarAnos(mesI, anoI, mesF, anoF):
         mesI = mesF
         mesF = aux
     elif anoF == anoI:
+        # Caso o ano seja o mesmo, verifica os meses e põe em ordem
         if mesI > mesF:
             aux = mesI
             mesI = mesF
@@ -95,8 +102,16 @@ def ordenarAnos(mesI, anoI, mesF, anoF):
 
 
 def setarIntervalo(arquivo, datas, posI, posF, cont):
-    datas[2] = f'{int(datas[2] + 1)}'
+    """Define a posição inicial e posição final do intervalo desejado no arquivo"""
 
+    # Se o mês final for 12 ele é substituído pelo primeiro mês do ano seguinte
+    if datas[2] == 12:
+        datas[2] = '1'
+        datas[3] = f'{int(datas[3] + 1)}'
+    else:
+        datas[2] = f'{int(datas[2] + 1)}'
+
+    # Percorre o arquivo para pegar as posições do intervalo
     for linha in arquivo:
         if (f'01/0{datas[0]}/{datas[1]}') in linha or (f'01/{datas[0]}/{datas[1]}') in linha:
             if posI == 0:
@@ -113,6 +128,7 @@ def setarIntervalo(arquivo, datas, posI, posF, cont):
     return posI,posF
 
 def visualizarTodos(mesI, anoI, mesF, anoF, arquivo, cab):
+    """Exibe todos os dados no intervalo determinado"""
     datas = ordenarAnos(mesI, anoI, mesF, anoF)
 
     posI = 0
@@ -129,6 +145,7 @@ def visualizarTodos(mesI, anoI, mesF, anoF, arquivo, cab):
     print()
         
 def visualizarPrec(mesI, anoI, mesF, anoF, arquivo):
+    """Exibe apenas a precipitação no intervalo determinado"""
     datas = ordenarAnos(mesI, anoI, mesF, anoF)
 
     posI = 0
@@ -145,6 +162,7 @@ def visualizarPrec(mesI, anoI, mesF, anoF, arquivo):
     print()
 
 def visualizarTemp(mesI, anoI, mesF, anoF, arquivo):
+    """Exibe apenas os dados de temperatura no intervalo determinado"""
     datas = ordenarAnos(mesI, anoI, mesF, anoF)
 
     posI = 0
@@ -161,6 +179,7 @@ def visualizarTemp(mesI, anoI, mesF, anoF, arquivo):
     print()
 
 def visualizarUmiVen(mesI, anoI, mesF, anoF, arquivo):
+    """Exibe apenas os dados de umidade e vento no intervalo determinado"""
     datas = ordenarAnos(mesI, anoI, mesF, anoF)
 
     posI = 0
@@ -177,8 +196,12 @@ def visualizarUmiVen(mesI, anoI, mesF, anoF, arquivo):
     print()
 
 def mesMaisChuvoso(arquivo):
+    """Pega o mês mais chuvoso de cada ano"""
     maisChuvoso = {}
     meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    
+    # Anos que não constam no arquivo de dados
+    # 1979, 1980, 1981, 1982, 2001 geram resultados errados
     anosInvalidos = [1985, 1986, 1987]
     for i in range(1961, 2017):
         maiorPre = 0
@@ -193,19 +216,76 @@ def mesMaisChuvoso(arquivo):
             posI, posF = setarIntervalo(arquivo, [j,i,j,i], 0, 0, 0)
 
             for dias in range(posI, posF):
+                # Faz a soma das precipitações do mês
                 soma += arquivo[dias][1]
+            # Compara com os maiores dados lidos naquele ano
             if soma > maiorPre:
                 maiorPre = soma
                 mesMaiorPre = meses[j-1]
         maisChuvoso[i] = (f'{i}: {mesMaiorPre} > {maiorPre:.2f}')
+
+    # Retorna um dicionário com os meses mais chuvosos por ano e sua precipitação
     return maisChuvoso
 
+def mediaMes(arquivo, meses, mes):
+    media = {}
+
+    cont = 1
+    numMes = contaMeses(meses, mes, cont)
+
+    for i in range(2006,2017):
+        cont = 0
+        mediaMin = 0 
+        somaMin = 0
+
+        if i == 2016 and numMes >= 7:
+            break
+
+        posI, posF = setarIntervalo(arquivo, [numMes, i, numMes, i], 0, 0, 0)
+
+        for dias in range(posI, posF):
+            somaMin += arquivo[dias][3]
+            cont += 1
+
+        mediaMin = somaMin/cont
+        media[(f'{mes}{i}')] = (f'{mediaMin:.2f}')
+
+    return media
+
+def contaMeses(meses, mes, cont):
+    """Pega o mês digitado e retorna seu número equivalente"""
+    for elem in meses:
+        if mes == elem:
+            numMes = cont
+            break
+        cont += 1
+    return numMes
+
+def gerarGrafico(mes, numMes, dicionario):
+    meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    valores = []
+    for i in range(2006,2017):
+        if i == 2016 and numMes >= 7:
+            break
+
+        chave = f'{mes}{i}'
+        valores.append(float(dicionario[chave]))
+    
+    anos = [a for a in range(2006, 2017)]
+
+    plt.bar(anos, valores)
+    plt.title(f'Médias mínimas para o mês de {meses[numMes-1]}')
+    plt.show()
+
+    return
 
 # ====================================== Programa principal ======================================
 
 # Carga dos dados nas variáveis
 dados = carregarDados('dados.csv')
 cab = salvarCabecalho('dados.csv')
+
+mesesInput = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro']
 
 while True:
     gerarMenu()
@@ -232,9 +312,36 @@ while True:
     else:
         print('Opção inválida!')
 
+#    print()
+#    print('> Mês/ano mais chuvosos:\n')
+#    maisChuvoso = mesMaisChuvoso(dados)
+#    for i in range(1961, 2017):
+#        if i in maisChuvoso:
+#            print(maisChuvoso[i])
+#    print()
+
+    mes = input('Digite um mês para exibir dados (ou 0 para sair): ').lower()
     print()
-    print('> Mês/ano mais chuvosos:\n')
-    maisChuvoso = mesMaisChuvoso(dados)
-    for i in range(1961, 2017):
-        if i in maisChuvoso:
-            print(maisChuvoso[i])
+    if mes == '0':
+        print('Fim do programa!')
+        break
+    while not(mes in mesesInput):
+        print('Mês inválido! Tente novamente.\n')
+        mes = input('Digite um mês para exibir dados: ').lower()
+    
+    medias = mediaMes(dados, mesesInput, mes)
+    numMes = contaMeses(mesesInput, mes, 1)
+    somaMin = 0
+    cont = 0
+    for i in range(2006, 2017):
+        if i == 2016 and numMes >= 7:
+            break
+        chave = f'{mes}{i}'
+        print(f'{mes}/{i}: {medias[chave]} °C')
+        somaMin += float(medias[chave])
+        cont += 1
+    print() 
+    
+    print(f'> Média mín. geral do período no mês de {mes}: {somaMin/cont:.2f} °C\n')
+
+    gerarGrafico(mes, numMes, medias)
